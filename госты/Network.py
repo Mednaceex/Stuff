19211de123156e45af60a8d8a36b897539d92846
Mednaceex <mednaceex@gmail.com>
2021-11-24 20:55:16 +0300

    3

--- госты/Network.py
+++ госты/Network.py
@@ -132,23 +132,26 @@ class NeuralNetwork:
 
                 for layer_index, layer in enumerate(self.layers):
                     length = len(self.layers)
-                    h = self.feedforward_to_layer(layer_index, x)
                     if layer_index == length - 1:
+                        h = self.feedforward_to_layer(layer_index, x)
                         for neuron_index, neuron in enumerate(layer):
                             for index, weight in enumerate(neuron):
                                 d_L_d_h[layer_index][neuron_index][index] = d * neuron.derivative(h) * weight
                                 d_L_d_w[layer_index][neuron_index][index] = d * neuron.derivative(h) * h[index]
                             d_L_d_b[layer_index][neuron_index] = d * neuron.derivative(h)
                     else:
+                        h = self.feedforward_to_layer(layer_index, x)
                         for neuron_index, neuron in enumerate(layer):
                             for index, weight in enumerate(neuron):
                                 dldh = 0
                                 for i, past_neuron in enumerate(self.layers[layer_index + 1]):
-                                    dldh += d_L_d_w[layer_index + 1][i][neuron_index]
+                                    dldh += d_L_d_h[layer_index + 1][i][neuron_index] * weight * neuron.derivative(h)
                                 d_L_d_h[layer_index][neuron_index][index] = dldh
-                                d_L_d_w[layer_index][neuron_index][index] = dldh * neuron.derivative(h) * h[index]
+                                dldw = 0
+                                for i, past_neuron in enumerate(self.layers[layer_index + 1]):
+                                    dldw += d_L_d_h[layer_index + 1][i][neuron_index] * h[index] * neuron.derivative(h)
+                                d_L_d_w[layer_index][neuron_index][index] = dldw
                             d_L_d_b[layer_index][neuron_index] = d * neuron.derivative(h)
-
 
                 # --- Обновляем веса и пороги
                 # Нейрон h1
